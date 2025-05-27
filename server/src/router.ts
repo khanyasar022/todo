16beb62fe90b962b7580d73dc3de1e7ff417aedc
yasar <yasarfarfat987@gmail.com>
2025-05-27 19:40:09 +0530

    get todos

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,6 @@
 import { initTRPC } from '@trpc/server';
 import { Context } from './context';
+import { z } from 'zod';
 
 const t = initTRPC.context<Context>().create();
 
@@ -7,7 +8,14 @@ export const router = t.router;
 export const publicProcedure = t.procedure;
 
 export const appRouter = router({
-  // We'll add procedures here later
+  todos: router({
+    // Get all todos
+    getAll: publicProcedure.query(async ({ ctx }) => {
+      return await ctx.prisma.todo.findMany({
+        orderBy: { createdAt: 'desc' },
+      });
+    }),
+  }),
 });
 
 export type AppRouter = typeof appRouter;
