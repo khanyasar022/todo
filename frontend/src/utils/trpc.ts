766d696746e703d04d38fd2fe6a4cc072d695525
yasar <yasarfarfat987@gmail.com>
2025-05-27 19:46:11 +0530

    trpc client config

---
+++ frontend/src/utils/trpc.ts
@@ -0,0 +1,10 @@
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { AppRouter } from '../../../server/src/router';
+
+export const trpc = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:5000/trpc',
+    }),
+  ],
+});
